30dba05453912872fb8a9b93523728f98f054ff3
Julien Torrielli <julieneragon@gmail.com>
2014-04-18 15:46:38 +0200

    Ajout des header Access-Control + mise en place de l'authenfication cote webservice + appli web tape directement sur le webservice

--- application/client/assets/javascripts/modules/cubbyhole/configs/cubbyholeConfig.js
+++ application/client/assets/javascripts/modules/cubbyhole/configs/cubbyholeConfig.js
@@ -1,6 +1,6 @@
 angular.module('CubbyHole').
 	config(['apiUrl', 'RestangularProvider', '$locationProvider', function(apiUrl, restangular, $location) {
-		
+
 		restangular.setBaseUrl(apiUrl);
 		$location.html5Mode(false);
 
--- application/server/routing/core.js
+++ application/server/routing/core.js
@@ -1,32 +1,14 @@
 var routing
 ,	navigation = require(global.paths.server + '/routing/routes/navigation')
-,   webservice = require(global.paths.server + '/routing/routes/webservice')
 ,   routing = {};
 
 routing.init = function(app) {
-
 
     app.get('/', navigation.redirect.home);
     app.get('/home', navigation.get.index);
     app.get('/manager', navigation.get.filemanager)
     app.get(/partials\/(.+)/, navigation.get.partial);
     app.get(/templates\/(.+)/, navigation.get.template);
-
-
-    /********************************[  REDIRECT  ]********************************/
-
-    app.get(/^\/api\/browse\/([0-9]+)$/, webservice.get.redirect);
-    app.get(/^\/api\/browse\/([0-9]+)\/(\/?.+)*/, webservice.get.redirect);
-    app.get(/^\/api\/download\/([0-9]+)\/(\/?.+)+/, webservice.get.redirect);
-    app.get(/^\/(\/?.+)*/, navigation.get.index);
-
-    app.post(/^\/api\/browse\/([0-9]+)$/, webservice.post.redirect);
-    app.post(/^\/api\/browse\/([0-9]+)(\/?.+)*\/$/, webservice.post.redirect);
-
-    app.put(/^\/api\/browse\/([0-9]+)\/(\/?.+)+/, webservice.put.redirect);
-
-    app.delete(/^\/api\/browse\/([0-9]+)\/(\/*.+)+/, webservice.delete.redirect);
-
 
 };
 
