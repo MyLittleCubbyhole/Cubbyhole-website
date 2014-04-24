0e6542fb41a24dd38320ba8be9eca08e51213249
Nicolas Gaignoux <gaignoux.nicolas@gmail.com>
2014-04-24 12:10:25 +0200

    [BUGFIX]

--- application/client/assets/javascripts/modules/config/constants/configConstants.js
+++ application/client/assets/javascripts/modules/config/constants/configConstants.js
@@ -1,3 +1,4 @@
 angular.module('Config').
-	constant('apiUrl', 'http://127.0.0.1:5150/api/').
+	// constant('apiUrl', 'http://127.0.0.1:5150/api/').
+	constant('apiUrl', 'http://www.pillowrc.com/api/').
 	constant('socketRoom', '/cubbyhole');
