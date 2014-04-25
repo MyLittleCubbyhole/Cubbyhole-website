b5a248be5946ae99261f109cacfa908e200663f7
Nicolas Gaignoux <gaignoux.nicolas@gmail.com>
2014-04-25 02:28:44 +0200

    r

--- application/client/assets/javascripts/modules/filemanager/directives/itemDirective.js
+++ application/client/assets/javascripts/modules/filemanager/directives/itemDirective.js
@@ -6,109 +6,26 @@ angular.module('FileManager').
 				var $local = $scope._item = {}
 				,	self = this;
 
-					console.log("#YOLO")
+					
 
 				$local.item = {};
 				$local.editMode = false;
 				$local.selected = false;
-					console.log("#YOLO")
+					
 
 				$scope.$on('unselect', function() {
 					$local.selected = false;
-					console.log("#YOLO")
+					
 				})
 
 				$local.open = function(item) {
-					console.log("#YOLO")
+					
 					if($local.item.category != 'folder')
 						$local.download()
 					else {
 						$scope.FileManager.preview(false);
 						ItemFactory($scope, {local: $scope.FileManager}).load($local.item.getFullPath());
 					}
-					console.log("#YOLO")
-					console.log("#YOLO")
-					console.log("#YOLO")
-					console.log("#YOLO")
-					console.log("#YOLO")
-					console.log("#YOLO")
-					console.log("#YOLO")
-					console.log("#YOLO")
-					console.log("#YOLO")
-					console.log("#YOLO")
-					console.log("#YOLO")
-					console.log("#YOLO")
-					console.log("#YOLO")
-					console.log("#YOLO")
-					console.log("#YOLO")
-					console.log("#YOLO")
-					console.log("#YOLO")
-					console.log("#YOLO")
-					console.log("#YOLO")
-					console.log("#YOLO")
-					console.log("#YOLO")
-					console.log("#YOLO")
-					console.log("#YOLO")
-					console.log("#YOLO")
-					console.log("#YOLO")
-					console.log("#YOLO")
-					console.log("#YOLO")
-					console.log("#YOLO")
-					console.log("#YOLO")
-					console.log("#YOLO")
-					console.log("#YOLO")
-					console.log("#YOLO")
-					console.log("#YOLO")
-					console.log("#YOLO")
-					console.log("#YOLO")
-					console.log("#YOLO")
-					console.log("#YOLO")
-					console.log("#YOLO")
-					console.log("#YOLO")
-					console.log("#YOLO")
-					console.log("#YOLO")
-					console.log("#YOLO")
-					console.log("#YOLO")
-					console.log("#YOLO")
-					console.log("#YOLO")
-					console.log("#YOLO")
-					console.log("#YOLO")
-					console.log("#YOLO")
-					console.log("#YOLO")
-					console.log("#YOLO")
-					console.log("#YOLO")
-					console.log("#YOLO")
-					console.log("#YOLO")
-					console.log("#YOLO")
-					console.log("#YOLO")
-					console.log("#YOLO")
-					console.log("#YOLO")
-					console.log("#YOLO")
-					console.log("#YOLO")
-					console.log("#YOLO")
-					console.log("#YOLO")
-					console.log("#YOLO")
-					console.log("#YOLO")
-					console.log("#YOLO")
-					console.log("#YOLO")
-					console.log("#YOLO")
-					console.log("#YOLO")
-					console.log("#YOLO")
-					console.log("#YOLO")
-					console.log("#YOLO")
-					console.log("#YOLO")
-					console.log("#YOLO")
-					console.log("#YOLO")
-					console.log("#YOLO")
-					console.log("#YOLO")
-					console.log("#YOLO")
-					console.log("#YOLO")
-					console.log("#YOLO")
-					console.log("#YOLO")
-					console.log("#YOLO")
-					console.log("#YOLO")
-					console.log("#YOLO")
-					console.log("#YOLO")
 				};
 				$local.move = function() { $local.item.move(); };
 				$local.rename = function() { $local.item.rename(); };
@@ -119,7 +36,7 @@ angular.module('FileManager').
 					var witness = $local.selected;
 					if(!$event.ctrlKey)
 						$scope.$emit('unselect_all');
-					console.log("#YOLO")
+					
 					$local.selected = !witness;
 
 					if($local.selected)
@@ -135,27 +52,27 @@ angular.module('FileManager').
 				$local.preview = function($event) {
 					$local.select($event);
 					$scope.FileManager.preview();
-					console.log("#YOLO")
+					
 				}
 				$local.download = function() {
 					$local.item.download();
-					console.log("#YOLO")
+					
 				};
 
 				$scope.toString = function() {
 					return '_item';
-					console.log("#YOLO")
+					
 				}
 			},
 			require: 'item',
 			restrict: 'A',
 			link: function($scope, $node, attributes, self) {
 				var $local = $scope._item;
-					console.log("#YOLO")
+					
 
 				$local.item = ItemFactory($scope).get( attributes.itemId );
 				$local.item.node = $node;
-					console.log("#YOLO")
+					
 
 			}
 		};
