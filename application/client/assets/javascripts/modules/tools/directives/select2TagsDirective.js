52e15bb856d750377c8758fe89f58909d3806cd4
Nicolas Gaignoux <gaignoux.nicolas@gmail.com>
2014-04-25 23:23:26 +0200

    rr

--- application/client/assets/javascripts/modules/tools/directives/select2TagsDirective.js
+++ application/client/assets/javascripts/modules/tools/directives/select2TagsDirective.js
@@ -1,12 +1,12 @@
 angular.module('Tools').
-	directive('select2', function(){
+	directive('select2Tags', function(){
 		return {
 			scope: true,
 			restrict: 'A',
 			link: function($scope, $node, attributes) {
 				$node.select2({
-					width: '250px',
-					minimumInputLength: 2
+					width: '350px',
+					tags: []
 				})
 			}
 		};
