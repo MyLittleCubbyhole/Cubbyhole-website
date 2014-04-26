f91d54404fba91f3f6982c80cb9a073b92994129
Nicolas Gaignoux <gaignoux.nicolas@gmail.com>
2014-04-26 13:58:44 +0200

    [FEATURE] taille

--- application/client/assets/javascripts/modules/filemanager/factories/uploaderFactory.js
+++ application/client/assets/javascripts/modules/filemanager/factories/uploaderFactory.js
@@ -9,8 +9,8 @@ angular.module('FileManager').
 			// files[data.id].context.$local.progress = Math.floor(data['percent']) + '%';
 			// files[data.id].context.$scope.$apply();
 			// console.log(Math.floor(data['percent']) + '%');
-			console.log(data['size'])
-			files[data.id].context.entity.size += data['size'];
+			console.log(data)
+			files[data.id].context.entity.size += data['chunkSize'];
 			files[data.id].context.$scope.$apply();
 			// console.log(files[data.id].context.$node)
 			var chunk = data['chunk'] * 524288
@@ -25,7 +25,7 @@ angular.module('FileManager').
 			// file.context.$local.progress = '100%';
 			// file.context.$scope.$apply();
 			// file.context.$scope.FileManager.refresh();
-			files[data.id].context.entity.size += data['size'];
+			files[data.id].context.entity.size += data['chunkSize'];
 			files[data.id].context.$scope.$apply();
 			// console.log(files[data.id].context.$node)
 
