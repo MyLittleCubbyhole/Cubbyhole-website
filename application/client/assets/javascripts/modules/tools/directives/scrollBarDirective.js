bded98ba08d3be5ac091e4ee5d0669dab5445610
Nicolas Gaignoux <gaignoux.nicolas@gmail.com>
2014-05-10 16:41:51 +0200

    [BUGFIX scroll bar]

--- application/client/assets/javascripts/modules/tools/directives/scrollBarDirective.js
+++ application/client/assets/javascripts/modules/tools/directives/scrollBarDirective.js
@@ -4,7 +4,15 @@ angular.module('Tools').
 			scope: true,
 			restrict: 'A',
 			link: function($scope, $node, attributes) {				
-				$node.mCustomScrollbar();
+				$node.mCustomScrollbar({  
+					scrollEasing:"easeOutCirc",  
+					mouseWheel:"auto",   
+					autoDraggerLength:true,   
+					advanced:{  
+						updateOnBrowserResize:true,   
+						updateOnContentResize:true   
+					} // removed extra commas  
+				});
 			}
 		};
 	});
