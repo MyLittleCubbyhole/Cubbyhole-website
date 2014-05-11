b5330dcfc308d575666945892977d36e84e7a782
Nicolas Gaignoux <gaignoux.nicolas@gmail.com>
2014-05-11 19:29:41 +0200

    [FEATURE] historique

--- application/client/assets/javascripts/modules/account/controllers/timelineController.js
+++ application/client/assets/javascripts/modules/account/controllers/timelineController.js
@@ -3,8 +3,33 @@ angular.module('Account').
 		var $local = $scope.Timeline = {};
 
 		UserFactory($scope).historic(function(data) {
-			for(var i = 0; i<data.length; i++)
+			var witness = true;
+			for(var i = 0; i<data.length; i++) {
 				data[i].date = new Date(data[i].date);
+				data[i].message = data[i].owner;
+				data[i].message += data[i].owner == 'You' ? ' have ' : ' has ';
+				witness = true;
+				switch(data[i].action) {
+					case 'delete': 
+						data[i].message += 'deleted ';
+					break;
+					case 'create':
+						data[i].message += data[i].itemType == 'folder' ? 'created ' : 'uploaded ';
+					break;
+					case 'share':
+						data[i].message += 'shared "/'+ data[i].fullPath.split('/').pop() + '" to ';
+					break;
+					case 'unshare':
+						data[i].message += 'stoped the sharing of "/'+ data[i].fullPath.split('/').pop() + '" ';
+						witness = false;
+					break;
+					case 'rename':
+						data[i].message += 'renamed "/'+ data[i].fullPath.split('/').pop() + '" to ';
+					break;
+				}
+				if(witness)
+					data[i].message += '"'+ data[i].name +'"';
+			}
 			$local.events = data;
 		});
 
