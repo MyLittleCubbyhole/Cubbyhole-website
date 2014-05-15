f7b600c287a6b817e28a4eef2abb432857565882
Nicolas Gaignoux <gaignoux.nicolas@gmail.com>
2014-05-15 11:41:00 +0200

    [bugfix]

--- application/client/assets/javascripts/modules/navigation/controllers/navigationController.js
+++ application/client/assets/javascripts/modules/navigation/controllers/navigationController.js
@@ -1,16 +1,19 @@
 angular.module('Navigation').
 	controller('NavigationController', ['$scope', '$window', '$location', 'UserFactory', '$document', function($scope, $window, $location, UserFactory, $document){
 		var $local = $scope.Navigation = {};
- 
+
 		$local.goto = function(path, container) {
 			path += (path == '/manager' && UserFactory($scope).get()) ? "?token=" + UserFactory($scope).get().token : "";
 			var pathView = path.slice(0, path.indexOf("#"));
 			var pathAngular = path.slice(path.indexOf("#"));
-
+			// console.log(path)
 			if(path.indexOf("/account") > -1)
 				path = pathView + "?token=" + UserFactory($scope).get().token + pathAngular;
 			// console.log(pathAngular)
-			if(path.indexOf("/home") > -1) {
+			
+			if(path.indexOf("/home") > -1 &&  $window.location.pathname == '/home') {
+
+				$location.path('/'+pathAngular.substring(1));
 				var target = angular.element(pathAngular)
 				,	container = container ? angular.element(container) : $document;
 				if(target.length>0) {
@@ -25,6 +28,7 @@ angular.module('Navigation').
 		};
 
 		$local.isSelected = function(pathname, anchor) {
+			anchor = anchor || '/';
 			return $location.$$path == anchor && $window.location.pathname == pathname;
 		}
 
@@ -38,6 +42,9 @@ angular.module('Navigation').
 		$local.isOnShared = function() {
 			return ($location.$$absUrl.indexOf("/shared/") > -1);
 		}
+
+		if($window.location.pathname == '/home' && $location.$$path.length>0)
+			$local.goto('/home#' + $location.$$path.substring(1), '#bloc-container')
 
 		$scope.toString = function() {
 			return 'Navigation';
