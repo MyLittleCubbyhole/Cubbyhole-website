104b70c0275342e25a7a11e98e49c6e63df2ea00
Julien Torrielli <julieneragon@gmail.com>
2014-05-17 15:40:14 +0200

    ajout config marcel

--- application/server/config/core.js
+++ application/server/config/core.js
@@ -1,12 +1,16 @@
 var config = {}
 ,	_ = require('lodash')
 ,	baseConfig = require(global.paths.server + '/config/jsons/config.json')
-,	envConfig = require(global.paths.server + '/config/jsons/' + global.configFile)
+,	envConfig = require(global.paths.server + '/config/jsons/' + global.configFile.env)
+,   developerConfig = global.configFile.developer != '' ? require(global.paths.server + '/config/jsons/' + global.configFile.developer) : ''
 ,	extended;
 
-config.init = function() { 
+config.init = function() {
 	extended = _.merge(baseConfig, envConfig);
-	return extended;
+    if(developerConfig != '')
+        extended = _.merge(extended, developerConfig);
+
+    return extended;
 }
 config.get = function() { return extended; }
 
